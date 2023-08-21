77c2119974b4de55ad1b3215cfac34d9844627e5
Prada <11524882+pradavc@users.noreply.github.com>
2023-08-21 10:50:56 +0200

    Env with specific versions (#44)
    * Update .env.example
    
    * Update specific versions

--- scripts/cmd/update-diva.sh
+++ scripts/cmd/update-diva.sh
@@ -4,4 +4,12 @@ exec_path=$1
 cd $exec_path
 
 git pull
+source .env.example
+
+sed -i.bak -e "s/^DIVA_VERSION *=.*/DIVA_VERSION=${DIVA_VERSION}/" .env
+sed -i.bak -e "s/^RELOADER_VERSION *=.*/RELOADER_VERSION=${RELOADER_VERSION}/" .env
+sed -i.bak -e "s/^OPERATOR_UI_VERSION *=.*/OPERATOR_UI_VERSION=${OPERATOR_UI_VERSION}/" .env
+sed -i.bak -e "s/^JAEGER_VERSION *=.*/JAEGER_VERSION=${JAEGER_VERSION}/" .env
+sed -i.bak -e "s/^VECTOR_VERSION *=.*/VECTOR_VERSION=${VECTOR_VERSION}/" .env
+
 docker compose up -d
