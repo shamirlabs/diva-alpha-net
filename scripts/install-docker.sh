ff23beb01fa9f878db7338d9916357bea7adb6df
longgeekk <125054780+longgeekk@users.noreply.github.com>
2023-09-20 06:56:15 +0800

    Install Docker using the get.docker.com script (#62)
    * Install Docker using the get.docker.com script
    
    Support for centos|fedora|rhel|ubuntu|debian|raspbian operating system
    
    * update script

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-sudo apt update -y
-sudo apt upgrade -y
-
-sudo apt-get install ca-certificates curl gnupg -y
-
-sudo install -m 0755 -d /etc/apt/keyrings
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update -y
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# From https://get.docker.com:
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
-exit 
+exit
