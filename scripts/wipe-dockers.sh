43eaad09fec19becddf56d897fe3f0d856855f10
Andrew Heiderscheit <heider.andrew@gmail.com>
2023-10-22 10:07:17 -0700

    Update wipe-dockers.sh
    Adding in missed geth and beacon image names

--- scripts/wipe-dockers.sh
+++ scripts/wipe-dockers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker stop diva validator vector node-exporter prometheus operator-ui jaeger reloader
-docker rm diva validator vector node-exporter prometheus operator-ui jaeger reloader
+docker stop diva validator vector node-exporter prometheus operator-ui jaeger reloader geth beacon grafana
+docker rm diva validator vector node-exporter prometheus operator-ui jaeger reloader geth beacon grafana
 docker rmi $(docker images -a -q) #TODO: Ideally this only targets the diva created images in the future
 docker system prune -f
